c503d5983092e8ba0444a61504432883a463039c
tmuelli <tm.muellertom@gmail.com>
2025-12-01 13:39:43 +0100

    AOC 2025 - Day 1

---
+++ day1_part2.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define DIAL_MAX 99
+
+#define LEFT 'L'
+#define RIGHT 'R'
+
+int main (int argc, char* argv[]) {
+    int rcode = 1;
+    FILE *pInput = NULL;
+    char line[256];
+    int dial = 50;
+    int count = 0;
+
+    pInput = fopen("./input/day1.txt", "r");
+
+    if (pInput == NULL) {
+        return -1;
+    }
+
+    while (fgets(line, sizeof(line), pInput)) {
+        char* pValue = &line[1];
+        int value = atoi(pValue);
+
+        if (!value) {
+            printf("error converting string to integer!\n");
+            rcode = 0;
+            break;
+        }
+
+        if (value >= (DIAL_MAX + 1)) {
+            int rem = value % (DIAL_MAX + 1);
+            count = count + ((value - rem) / 100);
+            value = rem;
+        }
+
+        if (line[0] == LEFT) {
+            if (dial - value < 0) {
+                if (dial != 0) {
+                    count++;
+                }
+                dial = DIAL_MAX + 1 - (value - dial);
+            } else {
+                dial -= value;
+            }
+        }
+
+        if (line[0] == RIGHT) {
+            if (dial + value > DIAL_MAX) {
+                dial = value - (DIAL_MAX + 1 - dial);
+                count++;
+                continue;
+            } else {
+                dial += value;
+            }
+        }
+
+        if (dial == 0) {
+            count++;
+        }
+    }
+
+    if (rcode) {
+        printf("The password is: %d\n", count);
+    } else {
+        printf("Error occured!\n");
+    }
+
+    fclose(pInput);
+    return 0;
+}
